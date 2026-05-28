b13fc1ec554f7c346643f746408f4540ad8eacb9
Sameer Kumar <sameerkr.dev@gmail.com>
2026-05-28 15:24:26 +0000

    yeahhhhh! i just wrote a rust server 😁

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello World" }));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    println!("Starting server on {}", addr);
+
+    let listener = match tokio::net::TcpListener::bind(addr).await {
+        Ok(listerner) => listerner,
+        Err(err) => {
+            panic!("Failed to bind TCP listener: {}", err);
+        }
+    };
+
+    if let Err(err) = axum::serve(listener, app).await {
+        panic!("server error: {}", err)
+    };
 }
